f0ea7201ac5951ae844d32a68047437c01522ab4
DziedMaroz <ilya.arkhanhelsky@gmail.com>
2012-02-19 15:31:40 +0300

    new file:   lab1/lab1/main.cpp

---
+++ lab1/lab1/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <windows.h>
+#include <string>
+using namespace std;
+
+int main (int argc, char* argv[])
+{
+    STARTUPINFO creatorStartupInfo, reporterStartupInfo;
+    PROCESS_INFORMATION creatorProcessInfo, reporterProcessInfo;
+
+    ZeroMemory (&creatorStartupInfo, sizeof(STARTUPINFO));
+    ZeroMemory (&reporterStartupInfo, sizeof(STARTUPINFO));
+    cout<<argv[0]<<endl;
+    string creatorConsole = string ("Z:\\home\\lucian\\University\\labOS\\lab1\\lab1-build-desktop\\creator.exe");
+    //string creatorConsole=string ("creator.exe");
+    cout<<"Binary filename for CREATOR output: ";
+    string params;
+
+    cin>>params;
+   // creatorConsole+=" ";
+   // creatorConsole+=params;
+    //const char* tmp
+    cout<<"Trying to start CREATOR process ("<<creatorConsole<<")\n";
+    if (!CreateProcess(creatorConsole.c_str(), params.c_str(), NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &creatorStartupInfo, &creatorProcessInfo))
+    {
+        cout<<"ERR: The new process (CREATOR) is not created\n";
+        return 100;
+    }
+    else
+    {
+        cout<<"lalala\n";
+    }
+    return 0;
+}
