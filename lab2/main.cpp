7d2bb52bb3c332545e10c0d74c00c6efd4f2e4be
DziedMaroz <ilya.arkhanhelsky@gmail.com>
2012-02-23 20:05:48 +0300

    deleted:    intput.in 	modified:   lab2.exe.so 	modified:   main.cpp

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -29,28 +29,40 @@ DWORD WINAPI mul (LPVOID arg)
 
 int main ()
 {
+	cout<<"Reading file input.in...\n";
 	FILE* file_in = fopen ("input.in","r");
+	if (!file_in)
+	{
+		cout<<"ERR:Something wrong with input file\n";
+		return 100;
+	}
 	int size = 0;
 	fscanf(file_in,"%d",&size);
+	cout<<"Matrix size: "<<size<<endl;
 	double** matrix = new double* [size];
+	cout<<"Matrix: \n";
 	for (int i=0;i<size;i++)
 	{
 		matrix[i]=new double [size];
 		for (int j=0;j<size;j++)
 		{
 			fscanf(file_in,"%lf",&matrix[i][j]);
+			printf ("%3.3lf ",matrix[i][i]);
 		}
+		printf ("\n");
 	}
-
+	cout<<"Reading vector...\n";
 	double* vector = new double [size];
 	for (int i=0;i<size;i++)
 	{
 		fscanf(file_in,"%lf",&vector[i]);
+		printf ("%3.3lf ",vector[i]);
 	}
+	printf ("\n");
 
 	HANDLE* hThreads = new HANDLE [size];
 	MulArgs* args = new MulArgs [size];
-	LPDWORD* threadsID = new LPDWORD [size];
+	DWORD* threadsID = new DWORD [size];
 	double* result = new double [size];
 	for (int i=0;i<size;i++)
 	{
@@ -60,7 +72,9 @@ int main ()
 		args[i].vector = vector;
 		args[i].resultCell = &result[i];
 
-		hThreads[i] = CreateThread (NULL,0, mul, (void*) &args[i], 0, threadsID[i]);
+		cout<<"Trying to start "<< i<<" thread\n";
+
+		hThreads[i] = (HANDLE) CreateThread (NULL,0, mul, (void*) &args[i], 0, &threadsID[i]);
 		if (!hThreads)
 		{
 			cout<<"Error on starting "<<i<<" thread...\n";
